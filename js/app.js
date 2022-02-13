c1e6d971e98ab96ce25ba6f3df918fd96e5b5125
Emran12 <emran6452@gmail.com>
2022-02-13 23:29:49 +0600

    pin matcher primarily completed

---
+++ js/app.js
@@ -0,0 +1,51 @@
+function getPin() {
+    let pin = Math.round(Math.random() * 10000);
+    if ((pin + '').length == 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+
+function generatePin() {
+    document.getElementById('display-pin').value = getPin();
+}
+
+document.getElementById('pin-generator').addEventListener('click', function () {
+    generatePin();
+})
+
+document.getElementById('button-container').addEventListener('click', function (event) {
+    const inputText = event.target.innerText;
+    let displayNumber, displayElement;
+    displayElement = document.getElementById('display-number');
+    if (!isNaN(inputText)) {
+        const displayText = displayElement.value;
+        displayNumber = displayText + inputText;
+        displayElement.value = displayNumber;
+    }
+
+    else if (inputText == 'Submit') {
+        let matchedPinMsg, unMatchedPinMsg;
+        matchedPinMsg = document.getElementById('matched-pin');
+        unMatchedPinMsg = document.getElementById('unmatched-pin');
+        const givenPin = document.getElementById('display-pin').value;
+        displayNumber = document.getElementById('display-number').value;
+        if (givenPin == displayNumber) {
+            matchedPinMsg.classList = 'notify';
+            unMatchedPinMsg.classList = 'notify, d-none';
+        }
+        else {
+            unMatchedPinMsg.classList = 'notify';
+            matchedPinMsg.classList = 'notify, d-none';
+        }
+    }
+    else if (inputText == 'C') {
+        displayElement.value = '';
+    }
+    else {
+        const displayText = displayElement.value;
+        displayElement.value = displayText.replace(displayText[displayText.length - 1], '');
+    }
+})
